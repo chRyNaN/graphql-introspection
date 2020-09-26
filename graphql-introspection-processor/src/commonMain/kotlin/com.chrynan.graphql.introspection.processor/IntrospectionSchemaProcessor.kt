d51cd6436a5adea05bb065deeff84440b2dca8ea
Chris <ckeenan@starry.com>
2020-09-26 17:28:12 -0400

    Created IntrospectionSchemaProcessor

---
+++ graphql-introspection-processor/src/commonMain/kotlin/com.chrynan.graphql.introspection.processor/IntrospectionSchemaProcessor.kt
@@ -0,0 +1,23 @@
+@file:Suppress("unused")
+
+package com.chrynan.graphql.introspection.processor
+
+import com.chrynan.graphql.introspection.core.IntrospectionSchema
+
+/**
+ * An interface for a class that processes an [IntrospectionSchema] and performs some task dependent on the
+ * [IntrospectionSchema].
+ */
+interface IntrospectionSchemaProcessor<R> {
+
+    /**
+     * Processes the provided [schema]. Depending on the implementation, this can invoke different actions including
+     * generating code. Returns the result [R] of the processing.
+     */
+    fun process(schema: IntrospectionSchema): R
+
+    /**
+     * A convenience operator function for calling [process].
+     */
+    operator fun invoke(schema: IntrospectionSchema): R = process(schema)
+}
