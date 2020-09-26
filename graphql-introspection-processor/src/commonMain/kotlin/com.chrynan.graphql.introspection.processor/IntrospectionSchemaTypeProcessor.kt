89f92a28c19898e44dd5c1cb16b3a3570fe80f10
Chris <ckeenan@starry.com>
2020-09-26 17:33:15 -0400

    Created IntrospectionSchemaTypeProcessor

---
+++ graphql-introspection-processor/src/commonMain/kotlin/com.chrynan.graphql.introspection.processor/IntrospectionSchemaTypeProcessor.kt
@@ -0,0 +1,24 @@
+@file:Suppress("unused")
+
+package com.chrynan.graphql.introspection.processor
+
+import com.chrynan.graphql.introspection.core.Type
+
+/**
+ * Processes a [Type] in the [process] function and performs some operation, such as, generating a Kotlin class from
+ * the type. This differs from the [IntrospectionSchemaProcessor] interface in that this interface operates on a single
+ * [Type] at a time.
+ */
+interface IntrospectionSchemaTypeProcessor<R> {
+
+    /**
+     * Processes the provided [type] using the provided [typeMap] which represents all of the available types for a
+     * schema.
+     */
+    fun process(type: Type, typeMap: Map<String, Type>): R
+
+    /**
+     * A convenience operator function for calling [process].
+     */
+    operator fun invoke(type: Type, typeMap: Map<String, Type>): R = process(type, typeMap)
+}
