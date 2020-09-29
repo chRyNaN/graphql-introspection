455bc3314a7f2e8dab3b84fd2b3be96f3d7201c3
Chris <ckeenan@starry.com>
2020-09-29 00:40:21 -0400

    Updated DefaultValueUtils and InputFieldSerializer and ArgumentSerializer

--- graphql-introspection-core/src/commonMain/kotlin/com.chrynan.graphql.introspection.core/ArgumentSerializer.kt
+++ graphql-introspection-core/src/commonMain/kotlin/com.chrynan.graphql.introspection.core/ArgumentSerializer.kt
@@ -1,8 +1,10 @@
 package com.chrynan.graphql.introspection.core
 
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerializationException
+import kotlinx.serialization.builtins.nullable
 import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.descriptors.*
 import kotlinx.serialization.encoding.*
@@ -36,6 +38,7 @@ object ArgumentSerializer : KSerializer<Argument> {
         }
     }
 
+    @Suppress("UNCHECKED_CAST")
     override fun deserialize(decoder: Decoder): Argument =
         decoder.decodeStructure(descriptor) {
             var name = ""
@@ -48,11 +51,16 @@ object ArgumentSerializer : KSerializer<Argument> {
             while (true) {
                 when (val index = decodeElementIndex(descriptor)) {
                     0 -> name = decodeStringElement(descriptor, 0)
-                    1 -> description = decodeStringElement(descriptor, 1)
+                    1 -> description = decodeNullableSerializableElement(descriptor, 1, String.serializer().nullable)
                     2 -> isDeprecated = decodeBooleanElement(descriptor, 2)
-                    3 -> deprecationReason = decodeStringElement(descriptor, 3)
+                    3 -> deprecationReason =
+                        decodeNullableSerializableElement(descriptor, 3, String.serializer().nullable)
                     4 -> typeRef = decodeSerializableElement(descriptor, 4, TypeRef.serializer())
-                    5 -> defaultValue = decodeSerializableElement(descriptor, 5, getSerializerFor(typeRef!!))
+                    5 -> defaultValue = decodeNullableSerializableElement(
+                        descriptor,
+                        5,
+                        getSerializerFor(typeRef!!) as DeserializationStrategy<DefaultValue?>
+                    )
                     CompositeDecoder.DECODE_DONE -> break
                     else -> throw SerializationException("Encountered an unexpected index when deserializing an Argument. index = $index")
                 }
--- graphql-introspection-core/src/commonMain/kotlin/com.chrynan.graphql.introspection.core/InputFieldSerializer.kt
+++ graphql-introspection-core/src/commonMain/kotlin/com.chrynan.graphql.introspection.core/InputFieldSerializer.kt
@@ -1,8 +1,10 @@
 package com.chrynan.graphql.introspection.core
 
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerializationException
+import kotlinx.serialization.builtins.nullable
 import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.descriptors.buildClassSerialDescriptor
@@ -38,6 +40,7 @@ object InputFieldSerializer : KSerializer<InputField> {
         }
     }
 
+    @Suppress("UNCHECKED_CAST")
     override fun deserialize(decoder: Decoder): InputField =
         decoder.decodeStructure(descriptor) {
             var name = ""
@@ -50,11 +53,16 @@ object InputFieldSerializer : KSerializer<InputField> {
             while (true) {
                 when (val index = decodeElementIndex(descriptor)) {
                     0 -> name = decodeStringElement(descriptor, 0)
-                    1 -> description = decodeStringElement(descriptor, 1)
+                    1 -> description = decodeNullableSerializableElement(descriptor, 1, String.serializer().nullable)
                     2 -> isDeprecated = decodeBooleanElement(descriptor, 2)
-                    3 -> deprecationReason = decodeStringElement(descriptor, 3)
+                    3 -> deprecationReason =
+                        decodeNullableSerializableElement(descriptor, 3, String.serializer().nullable)
                     4 -> typeRef = decodeSerializableElement(descriptor, 4, TypeRef.serializer())
-                    5 -> defaultValue = decodeSerializableElement(descriptor, 5, getSerializerFor(typeRef!!))
+                    5 -> defaultValue = decodeNullableSerializableElement(
+                        descriptor,
+                        5,
+                        getSerializerFor(typeRef!!) as DeserializationStrategy<DefaultValue?>
+                    )
                     CompositeDecoder.DECODE_DONE -> break
                     else -> throw SerializationException("Encountered an unexpected index when deserializing an Argument. index = $index")
                 }
