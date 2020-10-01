2549c8a659cd5200eb0634880dd864423b22a28c
Chris <ckeenan@starry.com>
2020-10-01 11:19:08 -0400

    Bumped version to 0.1.1

--- buildSrc/src/main/java/com.chrynan.graphql.introspection.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.graphql.introspection.buildSrc/LibraryConstants.kt
@@ -6,9 +6,9 @@ object LibraryConstants {
     const val group = "com.chrynan.graphql.introspection"
     const val owner = "chrynan"
     const val repoName = "graphql-introspection"
-    const val versionName = "0.1.0"
-    const val versionCode = 1
-    const val versionDescription = "Pre-release 0.1.0"
+    const val versionName = "0.1.1"
+    const val versionCode = 2
+    const val versionDescription = "Pre-release 0.1.1"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/graphql-introspection.git"
 }
